e6da21bc6e44192b56fe9de7501c2cddb7bdd007
chenqi <chen_q_i@163.com>
2017-07-06 11:41:45 +0800

    结构修改

--- src/cn/itcast/scm/service/SupplierService.java
+++
@@ -1,8 +0,0 @@
-package cn.itcast.scm.service;
-
-import cn.itcast.scm.entity.Dept;
-import cn.itcast.scm.entity.Supplier;
-
-public interface SupplierService {
-	 public int insertSupplier(Supplier supplier) throws Exception;	
-}
