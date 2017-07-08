64255670a74705533e4db0d4c5361c4e5f8f28da
陈奇 <892854990@qq.com>
2017-07-08 10:24:37 +0800

    queueDemo

---
+++ src/com/cn/readThinkingInJava/seventeen/QueueBehavior.java
@@ -0,0 +1,44 @@
+package com.cn.readThinkingInJava.seventeen;
+
+import com.cn.readThinkingInJava.net.mindview.util.Generator;
+
+import java.util.LinkedList;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.LinkedBlockingDeque;
+import java.util.concurrent.PriorityBlockingQueue;
+
+/**
+ * Created by Administrator on 2017/7/8 0008.
+ */
+public class QueueBehavior {
+    private static int count =10;
+    static <T> void test(Queue<T> queue , Generator<T> gen){
+        for (int i=0;i<count;i++)
+            queue.offer((gen.next()));
+        while(queue.peek()!=null)
+            System.out.print(queue.remove()+" ");
+        System.out.println("-------------------------------------------");
+    }
+    static class Gen implements Generator<String>{
+        String[] s = ("one tow three four five six seven eight night ten").split(" ");
+        int i;
+
+        @Override
+        public String next() {
+            return s[i++];
+        }
+    }
+
+    public static void main(String[] args) {
+        test(new LinkedList<String>(),new Gen());
+        test(new PriorityQueue<String>(),new Gen());
+        test(new ArrayBlockingQueue<String>(count),new Gen());
+        test(new ConcurrentLinkedQueue<String>(),new Gen());
+        test(new LinkedBlockingDeque<String>(),new Gen());
+        test(new PriorityBlockingQueue<String>(),new Gen());
+
+    }
+}
